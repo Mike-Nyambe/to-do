8a90604f03ae34fb51cc27ccaabd6f889458a9e8
Mike-Nyambe <marklinenyambe@gmail.com>
2024-04-07 00:19:44 +0200

    laravel

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'Main Page';
+});
+Route::get('/hello', function (){
+    return 'Hello World';
+});
+
+Route::fallback( function (){
+    return '404 Page';
+});
